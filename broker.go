662c3c57662c52ca3fdbc30d3d5db0ffcc81c320
Stephen Afam-Osemene <stephen.afam-osemene@engelvoelkers.com>
2020-06-04 10:17:20 +0200

    Use a main broker instead of controller

--- broker.go
+++ broker.go
@@ -1,4 +1,4 @@
-package controllers
+package janus
 
 import (
 	"log"
@@ -13,6 +13,8 @@ import (
 	"github.com/stephenafamo/janus/views"
 )
 
+type mid = func(http.Handler) http.Handler
+
 type logger interface {
 	Printf(format string, v ...interface{})
 }
@@ -22,18 +24,18 @@ var Logger logger = log.New(os.Stderr, "", log.LstdFlags)
 
 // CSRFMiddleware is the middleware that will be returned in c.RecommnededMiddlewares
 // It is exported to allow external change to the CSRF middleware
-var CSRFMiddleware mid
+var CSRFMiddleware middlewares.CSRF = middlewares.Nosurf{}
 
-type mid = func(http.Handler) http.Handler
-
-// Controller contains all our routes and handlers and can return a handler
+// Broker contains all our routes and handlers and can return a handler
 // for our http.Server
-type Controller struct {
+type Broker struct {
 	Domains   []string
 	Templates views.TemplateExecutor
 	Store     store.Store
 	Assets    http.FileSystem
 	Auth      auth.Authenticator
+
+	csrfMiddleware middlewares.CSRF
 }
 
 // WriteError is a helper to return an error
@@ -43,36 +45,42 @@ func WriteError(w http.ResponseWriter, r *http.Request, err error, code int) {
 }
 
 // SetDomains for our handler
-func (c *Controller) SetDomains(d []string) {
-	c.Domains = d
+func (b *Broker) SetDomains(d []string) {
+	b.Domains = d
 }
 
 // SetTemplates for our handler
-func (c *Controller) SetTemplates(t views.TemplateExecutor) {
-	c.Templates = t
+func (b *Broker) SetTemplates(t views.TemplateExecutor) {
+	b.Templates = t
 }
 
 // SetStore for our handler
-func (c *Controller) SetStore(s store.Store) {
-	c.Store = s
+func (b *Broker) SetStore(s store.Store) {
+	b.Store = s
 }
 
 // SetAssets for our handler
-func (c *Controller) SetAssets(s http.FileSystem) {
-	c.Assets = s
+func (b *Broker) SetAssets(s http.FileSystem) {
+	b.Assets = s
 }
 
 // SetAuth for our handler
-func (c *Controller) SetAuth(a auth.Authenticator) {
-	c.Auth = a
+func (b *Broker) SetAuth(a auth.Authenticator) {
+	b.Auth = a
+}
+
+// GetCSRFToken to use for subsequet requests
+func (b *Broker) GetCSRFToken(r *http.Request) string {
+	m := b.csrfMiddleware
+	if m == nil {
+		m = CSRFMiddleware
+	}
+	return m.Token(r)
 }
 
 // RecommendedMiddlewares for our handler
-func (c Controller) RecommendedMiddlewares() []mid {
-	UseCSRF := CSRFMiddleware
-	if UseCSRF == nil {
-		UseCSRF = middlewares.CSRF
-	}
+func (b Broker) RecommendedMiddlewares() []mid {
+
 	mids := []mid{
 		middleware.StripSlashes,
 		middleware.RequestID,
@@ -80,13 +88,13 @@ func (c Controller) RecommendedMiddlewares() []mid {
 		middleware.Logger,
 		middleware.Recoverer,
 		gziphandler.GzipHandler,
-		middlewares.CORSMiddleware(c.Domains),
-		UseCSRF,
+		middlewares.CORSMiddleware(b.Domains),
+		CSRFMiddleware.Middleware,
 	}
 
-	if c.Auth != nil {
+	if b.Auth != nil {
 		// Add the default auth middlewares
-		mids = append(mids, c.Auth.DefaultMiddlewares()...)
+		mids = append(mids, b.Auth.DefaultMiddlewares()...)
 	}
 
 	return mids
