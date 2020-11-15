09fa6bc708192d19629b1802e84ca4a9d287a99a
Stephen Afam-Osemene <stephen.afam-osemene@engelvoelkers.com>
2020-11-15 21:15:49 +0100

    add afero source

---
+++ views/source/afero.go
@@ -0,0 +1,55 @@
+package source
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/afero"
+)
+
+// AferoTemplates is an implementation of the Templates interface
+// based on the excellent packr library.
+type AferoTemplates struct {
+	Store      afero.Fs
+	Suffix     string // required suffix for template files
+	TrimSuffix bool   // should suffix be removed from template names?
+}
+
+// Walk imiplements the Templates interface
+func (p AferoTemplates) Walk(walkFunc func(string, http.File) error) error {
+	return afero.Walk(p.Store, ".", func(path string, info os.FileInfo, err error) error {
+
+		// Ignore hidden files (files that start with a period)
+		if strings.HasPrefix(info.Name(), ".") {
+			return nil
+		}
+
+		// Skip files that do not have the required suffix
+		if !strings.HasSuffix(path, p.Suffix) {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		file, err2 := p.Store.Open(path)
+		if err2 != nil {
+			return err2
+		}
+		defer file.Close()
+
+		if p.TrimSuffix {
+			path = strings.TrimSuffix(path, p.Suffix)
+		}
+
+		err = walkFunc(path, file)
+		if err != nil {
+			return fmt.Errorf("error in walkFunc: %w", err)
+		}
+
+		return nil
+	})
+}
