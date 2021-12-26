ac2a67ff09bc4902c3f46e1dc3aa66b3d7f33c28
Stephen Afam-Osemene <me@stephenafamo.com>
2021-12-26 10:24:54 +0100

    add some template functions by default

--- views/executor/template_functions.go
+++ views/executor/template_functions.go
@@ -9,12 +9,15 @@ import (
 )
 
 var funcMap = template.FuncMap{
-	"ToUpper":    strings.ToUpper,
-	"ToLower":    strings.ToLower,
-	"ToTitle":    strings.Title,
-	"Join":       strings.Join,
-	"TrimPrefix": strings.TrimPrefix,
-	"TrimSuffix": strings.TrimSuffix,
+	"ToUpper":        strings.ToUpper,
+	"ToLower":        strings.ToLower,
+	"ToTitle":        strings.Title,
+	"Join":           strings.Join,
+	"TrimPrefix":     strings.TrimPrefix,
+	"TrimSuffix":     strings.TrimSuffix,
+	"HTMLEscape":     template.HTMLEscaper,
+	"JSEscape":       template.JSEscaper,
+	"URLQueryEscape": template.URLQueryEscaper,
 	"Year": func() int {
 		year, _, _ := time.Now().Date()
 		return year
