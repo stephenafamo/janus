abf7d7e038f176dbed1b40180628addb8fba1fc3
Stephen Afam-Osemene <me@stephenafamo.com>
2023-11-17 18:00:38 +0000

    Use goose provider

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/go-chi/cors v1.2.1
 	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/justinas/nosurf v1.1.1
-	github.com/pressly/goose/v3 v3.15.2-0.20231031120933-8e088eb65bb8
+	github.com/pressly/goose/v3 v3.16.0
 	github.com/rubenv/sql-migrate v1.5.0
 	github.com/spf13/afero v1.9.5
 	github.com/stephenafamo/mailer v0.0.0-20220614171951-46ce34b56078
@@ -29,15 +29,17 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
-	golang.org/x/sync v0.4.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
--- migrator/goose/goose.go
+++ migrator/goose/goose.go
@@ -2,52 +2,30 @@ package goose
 
 import (
 	"context"
-	"database/sql"
 	"errors"
-	"io/fs"
 	"math"
 
 	"github.com/pressly/goose/v3"
 	"github.com/stephenafamo/janus/migrator"
 )
 
-func Get(db *sql.DB, dialect string, source fs.FS, opts ...goose.OptionsFunc) migrator.Migrator {
+func Get(provider *goose.Provider) migrator.Migrator {
 	return impl{
-		db:      db,
-		dialect: dialect,
-		source:  source,
-		options: opts,
+		provider: provider,
 	}
 }
 
 type impl struct {
-	db      *sql.DB
-	dialect string
-	source  fs.FS
-	options []goose.OptionsFunc
-}
-
-func (g impl) setup() error {
-	goose.SetBaseFS(g.source)
-
-	if err := goose.SetDialect(g.dialect); err != nil {
-		return err
-	}
-
-	return nil
+	provider *goose.Provider
 }
 
 func (g impl) Up(ctx context.Context, limit int) (int, error) {
-	if err := g.setup(); err != nil {
-		return 0, err
-	}
-
 	if limit == 0 {
 		limit = math.MaxInt64
 	}
 
 	for i := 0; i < limit; i++ {
-		err := goose.UpByOneContext(ctx, g.db, ".", g.options...)
+		_, err := g.provider.UpByOne(ctx)
 		if err != nil {
 			if errors.Is(err, goose.ErrNoNextVersion) {
 				return i, nil
@@ -60,18 +38,14 @@ func (g impl) Up(ctx context.Context, limit int) (int, error) {
 }
 
 func (g impl) Down(ctx context.Context, limit int) (int, error) {
-	if err := g.setup(); err != nil {
-		return 0, err
-	}
-
 	if limit == 0 {
 		limit = math.MaxInt64
 	}
 
 	for i := 0; i < limit; i++ {
-		err := goose.DownContext(ctx, g.db, ".", g.options...)
+		_, err := g.provider.Down(ctx)
 		if err != nil {
-			if errors.Is(err, goose.ErrNoCurrentVersion) {
+			if errors.Is(err, goose.ErrNoNextVersion) {
 				return i, nil
 			}
 			return i, err
